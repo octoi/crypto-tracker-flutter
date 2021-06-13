f3cb52e985781362b37f0dea0add77ebda2d5b1e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 13:19:00 +0530

    simple list

---
+++ lib/widgets/coin_list.dart
@@ -0,0 +1,22 @@
+import 'package:crypto/widgets/coin.dart';
+import 'package:flutter/material.dart';
+
+class CoinList extends StatelessWidget {
+  final List coins;
+
+  const CoinList({Key? key, required this.coins}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: coins.map((coin) {
+        return Coin(
+          name: coin?.name,
+          price: coin?.price,
+          image: coin?.image,
+          priceChange: coin?.price_change_percentage_24h,
+        );
+      }).toList(),
+    );
+  }
+}
