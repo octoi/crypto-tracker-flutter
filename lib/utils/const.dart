0ce3e1fe2ca522f1f1f30508b270a57beeddacbc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:38:09 +0530

    added api urls

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+// colors
 const Color appPrimaryColor = Color(0xFF101012);
 const Color appSecondaryColor = Color(0xFF171718);
 const Color appRed = Color(0xFFF00606);
 const Color appGreen = Color(0xFF13C783);
+
+// api urls
+const apiUrl =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+const apiCoinUrl = 'https://api.coingecko.com/api/v3/coins/';
