34977ed4588c3a3fd45742a793bb50814c5613ff
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 13:19:08 +0530

    added image

--- lib/widgets/coin.dart
+++ lib/widgets/coin.dart
@@ -4,8 +4,9 @@ class Coin extends StatelessWidget {
   final name;
   final price;
   final priceChange;
+  final image;
 
-  const Coin({Key? key, this.name, this.price, this.priceChange});
+  const Coin({Key? key, this.name, this.price, this.priceChange, this.image});
 
   @override
   Widget build(BuildContext context) {
