d36d6514da17a1d61db0a88d1afc749403f0a408
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:51:50 +0530

    some styles and used text field

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:crypto/utils/api.dart';
 import 'package:crypto/utils/const.dart';
+import 'package:crypto/widgets/SearchBar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -26,6 +27,17 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appPrimaryColor,
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [SearchBar(onChange: () {})],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
