df36c7ca4c6fdaaa3e1b3b7285f535f7447f9eb0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:42:22 +0530

    get all coins api

---
+++ lib/utils/api.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+import 'package:crypto/utils/const.dart';
+
+getAllCoins() async {
+  var url = Uri.parse(apiUrl);
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+}
