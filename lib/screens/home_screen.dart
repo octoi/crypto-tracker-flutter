411ed99546a63bce8dd60bc9503e8b604f3288c8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:46:37 +0530

    api fetching

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,27 @@
+import 'package:crypto/utils/api.dart';
 import 'package:crypto/utils/const.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _coins;
+
+  void getCoins() async {
+    var _coinData = await getAllCoins();
+    setState(() => _coins = _coinData);
+    print(_coins);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCoins();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
