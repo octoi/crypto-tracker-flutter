db941482fd77f6e3fb91731cc4048622ba07041c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 13:09:54 +0530

    search ui

--- lib/widgets/SearchBar.dart
+++ lib/widgets/SearchBar.dart
@@ -1,3 +1,5 @@
+import 'package:crypto/utils/const.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
@@ -7,6 +9,20 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return TextField(
+      decoration: InputDecoration(
+        fillColor: appSecondaryColor,
+        filled: true,
+        border: InputBorder.none,
+        hintText: 'search',
+        hintStyle: TextStyle(color: appWhite, fontSize: 18.0),
+        prefixIcon: Icon(
+          CupertinoIcons.search,
+          color: appWhite,
+          size: 20.0,
+        ),
+      ),
+      style: TextStyle(color: appWhite),
+    );
   }
 }
