19fad984d61fef96e9aa0ce9e284090e19725350
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:58:26 +0530

    changed color

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // colors
 const Color appPrimaryColor = Color(0xFF101012);
-const Color appSecondaryColor = Color(0xFF171718);
+const Color appSecondaryColor = Color(0xFF1e1e1f);
 const Color appRed = Color(0xFFF00606);
 const Color appGreen = Color(0xFF13C783);
 
