251cd123ce87193204d8987f4137e9c52a7bfdc2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 13:05:16 +0530

    colors

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -5,6 +5,7 @@ const Color appPrimaryColor = Color(0xFF101012);
 const Color appSecondaryColor = Color(0xFF1e1e1f);
 const Color appRed = Color(0xFFF00606);
 const Color appGreen = Color(0xFF13C783);
+const Color appWhite = Color(0xFFE2E2E2);
 
 // api urls
 const apiUrl =
