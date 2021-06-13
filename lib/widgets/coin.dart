066da9bad1ca4f9a2fb1ded9110eaf6bb8a83b00
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 13:38:45 +0530

    ui

--- lib/widgets/coin.dart
+++ lib/widgets/coin.dart
@@ -1,3 +1,4 @@
+import 'package:crypto/utils/const.dart';
 import 'package:flutter/material.dart';
 
 class Coin extends StatelessWidget {
@@ -10,8 +11,24 @@ class Coin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [Text(name)],
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      padding: EdgeInsets.symmetric(vertical: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: appSecondaryColor,
+      ),
+      child: ListTile(
+        leading: Image.network(image, height: 30.0),
+        title: Text(name, style: TextStyle(color: appWhite, fontSize: 20.0)),
+        trailing: Text(
+          '$priceChange',
+          style: TextStyle(
+            color: priceChange < 0 ? appRed : appGreen,
+            fontSize: 18.0,
+          ),
+        ),
+      ),
     );
   }
 }
