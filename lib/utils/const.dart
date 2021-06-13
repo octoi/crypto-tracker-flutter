d08465ace83420fc53003ee607ef6320102a2779
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:34:34 +0530

    colors

---
+++ lib/utils/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appPrimaryColor = Color(0xFF101012);
+const Color appSecondaryColor = Color(0xFF171718);
+const Color appRed = Color(0xFFF00606);
+const Color appGreen = Color(0xFF13C783);
