e8d017800bc3eca000823c1a47fff8f4c465ced5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-13 12:51:40 +0530

    sample search

---
+++ lib/widgets/SearchBar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  final Function onChange;
+
+  const SearchBar({Key? key, required this.onChange}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
